413cfd80874e8f6c37c718844c6efa27bf628f5e
chubei <914745487@qq.com>
2021-06-23 13:40:43 +0800

    builder/04-call-build

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -15,6 +15,8 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let builder_fields = builder_fields(&input.data);
     // Generate setters for all the fields.
     let builder_setters = builder_setters(&input.data);
+    // Generate build method.
+    let builder_build = builder_build(&name, &input.data);
 
     let expanded = quote! {
         #[derive(Default)]
@@ -24,6 +26,8 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
         impl #builder_name {
             #builder_setters
+
+            #builder_build
         }
 
         impl #name {
@@ -74,8 +78,8 @@ fn builder_fields(data: &Data) -> TokenStream {
 fn builder_setters(data: &Data) -> TokenStream {
     match *data {
         Data::Struct(ref data) => match data.fields {
-            Fields::Named(ref fileds) => {
-                let recurse = fileds.named.iter().map(|f| {
+            Fields::Named(ref fields) => {
+                let recurse = fields.named.iter().map(|f| {
                     let name = &f.ident;
                     let ty = &f.ty;
                     quote! {
@@ -86,6 +90,40 @@ fn builder_setters(data: &Data) -> TokenStream {
                 });
                 quote! {
                     #(#recurse)*
+                }
+            }
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    }
+}
+
+// Generated code looks like this:
+// ```rust
+// pub fn build(self) -> Result<Command, &'static str> {
+//     Ok(Command {
+//         executable: self.executable.ok_or("Missing field executable")?,
+//         args: self.args.ok_or("Missing field args")?,
+//     })
+// }
+// ```
+fn builder_build(name: &Ident, data: &Data) -> TokenStream {
+    match *data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => {
+                let recurse = fields.named.iter().map(|f| {
+                    let name = &f.ident;
+                    let message = format!("Missing field {}", name.as_ref().unwrap().to_string());
+                    quote! {
+                        #name: self.#name.ok_or(#message)?,
+                    }
+                });
+                quote! {
+                    pub fn build(self) -> Result<#name, &'static str> {
+                        Ok(#name {
+                            #(#recurse)*
+                        })
+                    }
                 }
             }
             _ => unimplemented!(),
