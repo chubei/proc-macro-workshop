bf605df4954ccfec8acdddfe2b1b48116f4a2393
chubei <chubei@oppentech.com>
2022-05-16 14:04:37 +0800

    seq/01-parse-header

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,28 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Ident, LitInt, parse::{Parse, ParseStream}, Result, Token, parse_macro_input, Expr};
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _ = parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    let expanded = quote! {
+
+    };
+
+    expanded.into()
+}
+
+struct Seq;
+
+impl Parse for Seq {
+    fn parse(input: ParseStream) -> Result<Self> {
+        input.parse::<Ident>()?;
+        input.parse::<Token!(in)>()?;
+        input.parse::<LitInt>()?;
+        input.parse::<Token!(..)>()?;
+        input.parse::<LitInt>()?;
+        input.parse::<Expr>()?;
+        Ok(Seq)
+    }
 }
